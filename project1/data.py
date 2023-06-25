e1840eea8481d913ecbbe27bffb9b970fe776c82
surajn581 <surajn581@gmail.com>
2023-06-26 03:09:24 +0530

    added logging for data.py

--- project1/data.py
+++ project1/data.py
@@ -1,12 +1,15 @@
 import requests
 import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(name = 'data.py')
+logger.setLevel(level = logging.DEBUG)
 
 DEFAULT = 'NOT FOUND'
 
 def get_data(lat, long):
     url     = f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&daily=sunrise,sunset&forecast_days=1&timezone=auto'
     res = requests.get(url).json()
-    print(res)
+    logger.debug(res)
     data = dict( sunrise = res.get('daily').get('sunrise')[0], sunset = res.get('daily').get('sunset')[0], timezone = res.get('timezone') )
     return data
 
@@ -14,5 +17,5 @@ def read_data(lat, long):
     try:
         return get_data(lat, long)
     except Exception as ex:
-        logging.error(ex)
+        logger.error(ex)
         return dict(sunrise = DEFAULT, sunset = DEFAULT, timezone = DEFAULT)
